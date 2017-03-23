3b3ae9dfd1c105aca67d1c191ffc0d16807bf90e
Baranov Pavel <pavelbaranow@gmail.com>
2017-03-23 20:37:49 +0300

    Small fixes

--- БАРАНОВ/LineTable/TLine.h
+++ БАРАНОВ/LineTable/TLine.h
@@ -5,6 +5,7 @@ const unsigned int MaxSize = 100;
 #include <string>    
 #include <iostream>  
 #include <iomanip>
+
 using namespace std;
 
 class Tline
@@ -25,7 +26,7 @@ public:
 	void printTable();
 	void printLine(int N);
 	void add(const string &newName, const string &newElem);		//have testa
-	void dell(int pos);
+	void dell(int pos);		//have testa
 	int searchID_argOne(const string &Elem);		//have testa
 	int searchID_argTwo(const string &Elem);		//have testa
 	string get_argOne_Of_Line_N(int N);		//have testa
@@ -33,9 +34,9 @@ public:
 };
 
 #endif 
-//Лишнее
+//Р›РёС€РЅРµРµ
 /*
 void add(const string &newName, const string &newElem, int pos);
-int popID();//удалить последнюю потом вернуть
-int topID();//вернуть
+int popID();//СѓРґР°Р»РёС‚СЊ РїРѕСЃР»РµРґРЅСЋСЋ РїРѕС‚РѕРј РІРµСЂРЅСѓС‚СЊ
+int topID();//РІРµСЂРЅСѓС‚СЊ
 */
